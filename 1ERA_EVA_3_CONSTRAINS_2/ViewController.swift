eae93c3870c370f2b7e77c6b6b9d221dde730723
danelly-dmh <danelly.dmh@gmail.com>
2017-03-04 20:01:10 -0700

    Constrains2

---
+++ 1ERA_EVA_3_CONSTRAINS_2/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  1ERA_EVA_3_CONSTRAINS_2
+//
+//  Created by TEMPORAL2 on 03/02/17.
+//  Copyright © 2017 comdanelly. All rights reserved.
+// <Hola mundo>
+// written by: DANELLY MONTAÑEZ HERNÁNDEZ
+// <INSTITUTO TECNOLÓGICO DE CHIHUAHUA II>
+// <Desarrollo de Aplicaciones para Dispositivos Móviles, Plataforma 1>
+// <03-05-2017>
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
